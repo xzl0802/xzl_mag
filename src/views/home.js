8051f881d541cd5c2f40d431bf50b15a7c8197c9
xzl <xzlperfect@163.com>
2020-05-14 16:03:34 +0800

    修改

--- src/views/home.js
+++ src/views/home.js
@@ -2,7 +2,19 @@
  * @Author: xzl 
  * @Date: 2020-05-14 14:44:11 
  * @Last Modified by: xzl
- * @Last Modified time: 2020-05-14 14:46:36
+ * @Last Modified time: 2020-05-14 15:37:29
  */
-
 import React from 'react';
+import './../styles/Home.css';
+function Home(){
+  return(
+    <div className="Home">
+    <header className="App-header">
+    </header>
+  </div>
+
+  );
+    
+}
+
+export default Home;
