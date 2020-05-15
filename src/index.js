cbdec69a0d62e9449f37bd641dbe47fa4c4571ad
xzl <xzlperfect@163.com>
2020-05-15 13:40:26 +0800

    完成界面整体布局

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './views/Home';
+import App from './views/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/views/home.js
+++ src/views/home.js
@@ -2,15 +2,14 @@
  * @Author: xzl 
  * @Date: 2020-05-14 14:44:11 
  * @Last Modified by: xzl
- * @Last Modified time: 2020-05-14 15:37:29
+ * @Last Modified time: 2020-05-15 11:20:53
  */
 import React from 'react';
 import './../styles/Home.css';
 function Home(){
   return(
     <div className="Home">
-    <header className="App-header">
-    </header>
+   
   </div>
 
   );
