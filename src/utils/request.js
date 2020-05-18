12fb97e08abe86068b23ceb9a89e15e1e05a55a5
xzl <xzlperfect@163.com>
2020-05-18 15:20:26 +0800

    添加 axios

---
+++ src/utils/request.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: xzl 
+ * @Date: 2020-05-18 14:29:30 
+ * @Last Modified by: xzl
+ * @Last Modified time: 2020-05-18 14:49:22
+ */
+
+ import axios from  'axios';
+ 
--- src/views/login.js
+++ src/views/login.js
@@ -2,17 +2,18 @@
  * @Author: xzl 
  * @Date: 2020-05-18 14:07:50 
  * @Last Modified by: xzl
- * @Last Modified time: 2020-05-18 14:16:12
+ * @Last Modified time: 2020-05-18 14:51:23
  */
 import React  from 'react';
 
 class Login extends React.Component(){
  constructor(){
-     
+     super(props);
  }
- 
+
 render() {
-return(<div>
+return(
+<div>
     
 </div>)  
 }
