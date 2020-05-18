d62a23bf177274797c0cc565f7e35f9a886d9d39
xzl <xzlperfect@163.com>
2020-05-18 14:16:41 +0800

    新增登录界面

---
+++ src/views/login.js
@@ -0,0 +1,23 @@
+/*
+ * @Author: xzl 
+ * @Date: 2020-05-18 14:07:50 
+ * @Last Modified by: xzl
+ * @Last Modified time: 2020-05-18 14:16:12
+ */
+import React  from 'react';
+
+class Login extends React.Component(){
+ constructor(){
+     
+ }
+ 
+render() {
+return(<div>
+    
+</div>)  
+}
+}
+
+
+
+export default Login;
