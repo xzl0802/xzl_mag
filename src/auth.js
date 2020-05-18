455b551e6d09dbec3b758ceb48899fabd7535bf2
xzl <xzlperfect@163.com>
2020-05-18 16:50:11 +0800

    提交

---
+++ src/auth.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: xzl 
+ * @Date: 2020-05-18 15:26:23 
+ * @Last Modified by: xzl
+ * @Last Modified time: 2020-05-18 15:34:44
+ */
+
+import React, { Component } from 'react';
+import {Redirect,Route}from "react-router-dom";
